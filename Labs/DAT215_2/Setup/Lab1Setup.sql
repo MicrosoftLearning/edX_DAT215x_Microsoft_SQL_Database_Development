08be0bc253f8a070e08a1930e9e2077021afa9b1
Prateek Chanda <prateekkol21@gmail.com>
2016-12-15 09:08:40 +0530

    Updating SOLUTION FOR LAB1

--- Labs/DAT215_2/Setup/Lab1Setup.sql
+++ Labs/DAT215_2/Setup/Lab1Setup.sql
@@ -1,3 +1,59 @@
 CREATE SCHEMA Reports;
 GO
 
+/*Challenge 1 */
+CREATE PROC Reports.GetProductColors
+AS
+BEGIN
+     
+      SELECT DISTINCT Color
+      FROM SalesLT.Product
+      WHERE Color is NOT NULL
+
+
+END
+
+
+
+CREATE PROC Reports.GetProductsAndModels
+AS
+BEGIN
+       SELECT ProductID, Name, ProductNumber, SellStartDate,SellEndDate,Color  
+       FROM SalesLT.Product
+       Order by ProductID
+       
+       SELECT ProductModelID 
+       FROM SalesLT.ProductModel
+       Order by ProductModelID
+       
+       SELECT Description  
+       FROM SalesLT.ProductDescription
+
+
+
+END
+
+
+/* Challenge 2 */
+
+CREATE PROC Reports.GetProductsByColor @Color nvarchar(10)
+AS
+BEGIN
+      
+         
+		    SELECT ProductID, Name, ListPrice  as Price, Color, Size 
+		    FROM SalesLT.Product
+		     WHERE Color = @Color
+		    Order by Name
+        
+
+	 if( @Color is NULL)
+		 BEGIN
+		      SELECT ProductID, Name, ListPrice  as Price, Color, Size 
+		      FROM SalesLT.Product
+		      WHERE Color is NULL
+		      Order by Name
+    END
+
+END
+
